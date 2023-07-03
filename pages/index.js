29c566ce2ff127840129da9a0a447bd93cbc86ef
ductam2909 <66300515+ductam2909@users.noreply.github.com>
2023-07-03 22:17:13 +0700

    dev

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import axios from "axios"
+import { useEffect } from "react"
 
 export default function Home () {
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/tours').then((res)=>{
+      console.log(res);
+    }).catch(()=>{
+      console.log("lỗi")
+    })
+  })
   return (
     <div>
-      Home page
+      Home page 3333
     </div>
   )
 }
